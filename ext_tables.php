13df722a3bed2154745a4730aed0a0434556e449
Daniel Huf <huf.daniel@gmail.com>
2019-09-11 12:56:57 +0200

    [TASK] Small changes + v2.0.0 as only v9

--- ext_tables.php
+++ ext_tables.php
@@ -1,12 +1,9 @@
 <?php
 defined('TYPO3_MODE') || die();
 
-$boot = function ($_EXTKEY) {
+(function ($extKey) {
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_variables_marker', 'EXT:variables/Resources/Private/Language/locallang_csh_tx_variables_marker.xlf');
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_variables_marker');
 
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Content Variables');
-};
-
-$boot($_EXTKEY);
-unset($boot);
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($extKey, 'Configuration/TypoScript', 'Content Variables');
+})('variables');
