59437df0c26ac833bf33bf121ff7daf4ebb3b172
Fabien Udriot <fabien@omic.ch>
2022-06-16 12:39:11 +0200

    Release 3.0.0 - <https://github.com/visol/ext-variables/compare/2.0.0...3.0.0>

---
+++ CHANGELOG.md
@@ -0,0 +1,10 @@
+## 3.0.0 (2022-06-16)
+
+[TASK] php annotation
+[BUGFIX] Fix namenspace
+[TASK] Add Services yaml
+[TASK] Increase TYPO3 v11 compatibility
+[TASK] Rename composer name
+[TASK] Small changes + v2.0.0 as only v9
+[TASK] Add .gitignore file
+[TASK] TYPO3 9 compatibility
--- ext_emconf.php
+++ ext_emconf.php
@@ -1,27 +1,10 @@
 <?php
 
-/***********************************************************************
- * Extension Manager/Repository config file for ext "variables".
- ***********************************************************************/
-
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Content Variables',
     'description' => 'Search and replace strings recursively after page generation using user-managed replacement definitions.',
-    'category' => 'misc',
-    'shy' => 0,
-    'version' => '2.0.0',
-    'state' => 'beta',
-    'author' => 'Xavier Perseguers',
-    'author_email' => 'xavier@causal.ch',
-    'author_company' => 'Swisscom (Schweiz) AG',
-    'constraints' => [
-        'depends' => [
-            'typo3' => '9.5.0-9.5.99',
-            'php' => '7.0.0-7.2.99',
-        ],
-        'conflicts' => [
-        ],
-        'suggests' => [
-        ],
-    ]
+    'state' => 'stable',
+    'author' => 'Jonas Renggli',
+    'author_email' => 'jonas.renggli@visol.ch',
+    'author_company' => 'visol - digitale Dienstleistungen GmbH',
 ];
