cce07fd72abb3b08bee5b12104979dbb98f6fac9
Daniel Huf <daniel.huf@visol.ch>
2024-11-08 15:44:32 +0100

    chore: Remove code for v11

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'Swisscom (Schweiz) AG',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-12.4.99',
+            'typo3' => '12.4.99',
             'php' => '8.1.0-8.3.99',
         ],
         'conflicts' => [
