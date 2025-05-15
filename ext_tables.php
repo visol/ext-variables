f3f9bd7739f6adbbb154567730dc1f536c2f5c94
Christian Wolfram <c.wolfram@chriwo.de>
2025-05-15 09:16:28 +0000

    [TASK] Migrate CSH description into TCA column property description (#33)
    see: https://docs.typo3.org/m/typo3/reference-coreapi/12.4/en-us/ApiOverview/ContextSensitiveHelp/

--- ext_tables.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
-
-defined('TYPO3') || die();
-
-(static function ($extKey) {
-    ExtensionManagementUtility::addLLrefForTCAdescr('tx_variables_marker', 'EXT:variables/Resources/Private/Language/locallang_csh_tx_variables_marker.xlf');
-})('variables');
